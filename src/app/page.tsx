538e03f924b1aa46e9d022e7d306ef8a69041973
vikrantsingh22 <vikrantsingh202004@gmail.com>
2024-03-31 08:17:44 +0530

    Minor change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,14 +13,13 @@ export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-center gap-5">
       <div className="flex items-center gap-4">
-        <Image src={logo} alt="FlowBrain logo" width={100} height={100} />
-        <span className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+        <Image src={logo} alt="FlowBrain logo" width={200} height={150} />
+        {/* <span className="text-4xl font-extrabold tracking-tight lg:text-5xl">
           FlowBrain
-        </span>
+        </span> */}
       </div>
       <p className="max-w-prose text-center">
-        An intelligent note-taking app with AI integration, built with OpenAI,
-        Pinecone, Next.js, Shadcn UI, Clerk, and more.
+        Welcome to LearnBlocks <br /> I am an AI bot to help in learning .
       </p>
       <Button size="lg" asChild>
         <Link href="/notes">Open</Link>
