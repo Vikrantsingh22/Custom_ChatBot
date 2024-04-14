81aad76cbf552b0a99709dd07e9cfb29ebb30fe1
vikrantsingh22 <vikrantsingh202004@gmail.com>
2024-04-14 07:31:54 +0530

    Minor changes

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -4,7 +4,7 @@ import { auth } from "@clerk/nextjs";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "LearnBlocks - Notes",
+  title: "ThinkLabs - Notes",
 };
 
 export default async function NotesPage() {
